4e1fa537274e5eb6b3e48d99628fd14e9fcd912e
Dorae <86833913+Toperlock@users.noreply.github.com>
2024-03-01 12:12:32 +0800

    Update save.py

--- 案例代码/小说网站/likeRead/save.py
+++ 案例代码/小说网站/likeRead/save.py
@@ -7,5 +7,5 @@ def save(book_name,store):
         json.dump(store,fp,ensure_ascii=False)
     with open("%s.txt"%book_name,"w",encoding='utf-8')as fp:
         for i in store:
-            fp.write(i["title"])
+            fp.write('  '+i["title"])
             fp.write(i["store"]+'\n')
