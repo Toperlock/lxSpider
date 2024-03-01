17e0bc1fdea69db8f097e8fcdcda53461819cae9
Dorae <86833913+Toperlock@users.noreply.github.com>
2024-03-01 12:13:45 +0800

    Update search.py

--- 案例代码/小说网站/likeRead/search.py
+++ 案例代码/小说网站/likeRead/search.py
@@ -1,31 +1,36 @@
-from lxml import etree
-import requests
-import win32clipboard as wc
-import win32con
+import requests as rts
+from bs4 import BeautifulSoup as Bp
+import re
+
+def reqdata(key,url):
+    r = rts.post(url,{'searchkey':key})
+    r.raise_for_status()
+    r.encoding = r.apparent_encoding
+    return r.text
+
+def handledata(key,url):
+    re_list = []
+    soup = Bp(reqdata(key,url),'html.parser')
+    data = soup.find_all('a')
+    for i in data:
+        try:
+            if re.findall(key,i.text): #根据关键字进行筛选
+                '''if i['href'][:4] == 'http':
+                    re_list.append(i.text + ':' + i['href'])'''
+                re_list.append(i.text + ':' +'https://www.piaotia.com/' + i['href'])
+        except:
+            continue
+
+    if len(re_list):
+        for j in range(len(re_list)):
+            print(str(j)+":"+re_list[j])
+        print("-"*50)
+        print("请复制下载链接到浏览器下载")
+    else:
+        print('未找到相关小说')
 
 def search(name):
-    search_web = ["http://www.zxcs.info/index.php?keyword=%s"%name]
-    html = etree.HTML(requests.get(search_web[0]).content.decode('utf-8',errors='ignore'))
-    title = html.xpath('//dl[@id="plist"]/dt/a//text()')
-    url = html.xpath('//dl[@id="plist"]/dt/a/@href')
-    if title == []:
-        return False
-    for i in range(len(title)):
-        title[i] = title[i].replace('《','').split('》')[0]
-        print(str(i)+":"+title[i])
-    print("-"*50)
-    index = input('请输入要下载的小说前方的序号：')
-
-    html_deta = etree.HTML(requests.get(url[int(index)]).content.decode('utf-8',errors='ignore'))
-    deta_src = 'http://www.zxcs.info'+html_deta.xpath('//p[@class="filetit"]/a/@href')[0]
-
-    html_donw = etree.HTML(requests.get(deta_src).content.decode('utf-8',errors='ignore'))
-    donw_src = html_donw.xpath('//span[@class="downfile"]/a/@href')
-    for src in donw_src:
-        if(src.split('.')[1]=='zxcsdown'):
-            print(src)
-            wc.OpenClipboard()
-            wc.EmptyClipboard()
-            wc.SetClipboardData(win32con.CF_UNICODETEXT, src)
-            wc.CloseClipboard()
-    return True
+    key = name
+    #url = "https://www.xbiquge.la/modules/article/waps.php"
+    url = "https://www.piaotia.com/modules/article/search.php"
+    handledata(key,url)
