8d0550d84a6af60c71a4ba2044fd1309af52e288
Dorae <86833913+Toperlock@users.noreply.github.com>
2024-03-01 12:10:55 +0800

    Update chapterList.py

--- 案例代码/小说网站/likeRead/chapterList.py
+++ 案例代码/小说网站/likeRead/chapterList.py
@@ -7,7 +7,8 @@ import query
 import json
 
 HEADER = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
 }
 
 with open('static/rules.json', 'r', encoding='utf-8')as fp:
@@ -32,6 +33,7 @@ def classify(url):
 def analysis(webName,url):
     html = etree.HTML(requests.get(url,HEADER).content.decode(rules[webName]['code'],errors='ignore'))
     title = html.xpath(rules[webName]['title'])
+    title = [t for t in title if '\xa0' not in t]
     urlList = html.xpath(rules[webName]['urlList'])
     book_name = html.xpath(rules[webName]['book_name'])[0].strip()
     #判断，判断该小说是否保存过
@@ -48,6 +50,8 @@ def analysis(webName,url):
             store_old = query_all["store"]
         else:
             query_all = query.query(book_name,title,urlList,True)
+    if webName == 'xbiquge': # xbiquge 的最新章节舍去，按需修改 注：中断后需去掉索引值才能与原文件继续保存
+        title = title[:]
     print("小说名称：%s"%book_name)
     print("小说章节：%d章"%(len(title)+len(store_old)))
     print("已存章节：%d章"%len(store_old))
@@ -55,10 +59,26 @@ def analysis(webName,url):
     print("-----开始保存-----")
     realm = rules[webName]['realm']
     store_all = []
-    for i in range(len(title)):
-        store = {
-            "title":title[i].strip(),
-            "urlList":realm+urlList[i]
-        }
+    for i in range(len(title)): # xbiquge 的最新章节舍去，按需修改 注：中断后需去掉索引值才能与原文件继续保存
+        if webName == 'ibswtan':
+            store = {
+                "title":title[i].strip(),
+                "urlList":realm+url.split("com")[1]+urlList[i]
+            }
+        elif webName == 'piaotia':
+            store = {
+                "title":title[i].strip(),
+                "urlList":url+urlList[i]
+            }
+        elif webName == 'xbiquge':
+            store = {
+                "title":title[i].strip(), # xbiquge 的最新章节舍去，按需修改 注：中断后需去掉索引值才能与原文件继续保存
+                "urlList":url+urlList[i]
+            }
+        else:
+            store = {
+                "title":title[i].strip(),
+                "urlList":realm+urlList[i]
+            }
         store_all.append(store)
     chapter.classify(book_name,webName,store_all,store_old)
